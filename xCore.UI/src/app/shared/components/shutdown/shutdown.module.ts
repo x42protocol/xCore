b489ab382458b2de1ea8eedbc4241a9626457426
Dennis <41244965+DennisAMenace@users.noreply.github.com>
2020-11-19 14:44:57 -0700

    Fixed exit button

--- xCore.UI/src/app/shared/components/shutdown/shutdown.module.ts
+++ xCore.UI/src/app/shared/components/shutdown/shutdown.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShutdownComponent } from './shutdown.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   exports: [ShutdownComponent],
   declarations: [ShutdownComponent],
 })
